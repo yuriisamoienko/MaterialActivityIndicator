9a372a2d3cc148eeac0c0e6439cba9b9866e8a22
Yurii Samoienko <yurii.samoienko@gmail.com>
2021-07-26 11:31:29 +0300

    added Carthage support

---
+++ MaterialActivityIndicator/MaterialActivityIndicator.h
@@ -0,0 +1,18 @@
+//
+//  MaterialActivityIndicator.h
+//  MaterialActivityIndicator
+//
+//  Created by Yurii Samoienko on 26.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MaterialActivityIndicator.
+FOUNDATION_EXPORT double MaterialActivityIndicatorVersionNumber;
+
+//! Project version string for MaterialActivityIndicator.
+FOUNDATION_EXPORT const unsigned char MaterialActivityIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialActivityIndicator/PublicHeader.h>
+
+
